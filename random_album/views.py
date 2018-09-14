840b798344536ab868fe115760a516e8b3b322f6
Anna Derengowski <annaderengowski@gmail.com>
2018-09-13 22:35:59 -0500

    started breakdown for the other decade lists

--- random_album/views.py
+++ random_album/views.py
@@ -8,9 +8,42 @@ from bs4 import BeautifulSoup
 def index(request):
 	#get_1990s_albums()
 	
+	urls = [
+		'https://pitchfork.com/features/lists-and-guides/the-200-best-albums-of-the-1960s/',
+		'https://pitchfork.com/features/lists-and-guides/5932-top-100-albums-of-the-1970s/',
+		'https://pitchfork.com/features/lists-and-guides/the-200-best-albums-of-the-1980s/',
+		'https://pitchfork.com/features/lists-and-guides/7710-the-top-200-albums-of-the-2000s-20-1/',
+	]
+
+	get_album_dict()
 
 	return render(request, 'random_album/index.html', {})
 
+def get_album_dict():
+	url = 'https://pitchfork.com/features/lists-and-guides/7710-the-top-200-albums-of-the-2000s-20-1/'
+	response = get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	contents = soup.findAll('div', {'class': 'list-blurb blurb-container container-fluid'})
+
+	albums = {}
+
+	for album in contents:
+		#album.h3.find('span', class_ = 'lister-item-year text-muted unbold')
+		rank = album.div.find('div', class_ = 'rank').text
+		artist = album.a.text
+		album_title = album.h2.text
+		year = album.h3.text
+		blurb = album.find('div', class_ = 'contents').p.text
+
+		albums[rank] = {
+			'artist': artist,
+			'album': album_title,
+			'year': year,
+			'blurb': blurb
+		}
+
+	print(albums)
+	#print(contents[0].li.text)
 
 def get_1990s_albums():
 	url = 'https://pitchfork.com/features/lists-and-guides/5923-top-100-albums-of-the-1990s/'
