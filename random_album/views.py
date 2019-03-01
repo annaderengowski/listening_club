349f2ea6c5b5758e053f91038a4e932a0d42de68
Anna <annaderengowski@gmail.com>
2019-02-28 21:13:13 -0600

    Form for random album to limit by decade

--- random_album/views.py
+++ random_album/views.py
@@ -9,6 +9,7 @@ from requests import get
 from bs4 import BeautifulSoup
 
 from random_album.models import Album
+from random_album.forms import RandomAlbumForm
 
 class AlbumListView(View):
 
@@ -20,19 +21,37 @@ class AlbumListView(View):
 class RandomAlbumView(View):
 
 	def get(self, request):
-		context = {}
-		q = Album.objects.all()
-		count = q.count()
-		random_album = q[random.randint(1, count+1)]
-		context['random_album'] = random_album
-		return render(request, 'random_album/random.html', context)
+		form = RandomAlbumForm()
+		return render(request, 'random_album/random_form.html', {'form': form})
 
 	def post(self, request):
-		pass
+		context = {}
+		form = RandomAlbumForm(request.POST)
+		if form.is_valid():
+			# Filter queryset by chosen decade
+			decade = form.cleaned_data['decade']
+			if decade == 'any':
+				q = Album.objects.all()
+			else:
+				start_year = int(decade)
+				end_year = start_year + 9
+				q = Album.objects.filter(year__gte=start_year,
+										 year__lte=end_year)
+
+			# Select a random ID from the queryset
+			count = q.count()
+			random_album = q[random.randint(1, count+1)]
+			context['random_album'] = random_album
+
+			return render(request,
+				'random_album/random.html',
+				{'album': random_album})
+		else:
+			pass
+			#TODO Send to error page
 
 
 def index(request):
 	context = {}
 
 	return render(request, 'random_album/index.html', context)
-
